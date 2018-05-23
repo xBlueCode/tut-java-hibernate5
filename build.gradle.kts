3151196459336729fdcc81ed12b7bb75570459b3
xBlueCode <xbluecode@localhost.localdomain>
2018-05-23 03:23:18 +0300

    Add C3P0 Connection pool support

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     compile("org.hibernate:hibernate-agroal:5.3.0.Final")
+    compile("org.hibernate:hibernate-c3p0:5.3.0.Final")
     compile("com.h2database:h2:1.4.197")
     compile("javax.transaction:jta:1.1")
 
